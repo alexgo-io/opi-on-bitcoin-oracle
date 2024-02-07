413bb6718783d3fce654246068298ee3835e917a
Zitao Xiong <caoer115@gmail.com>
2024-02-07 16:12:29 +0800

    chore: bitcond config change

--- docker/bitcoind/docker-entrypoint.sh
+++ docker/bitcoind/docker-entrypoint.sh
@@ -5,6 +5,7 @@ export BITCOIN_RPC_PORT=${BITCOIN_RPC_PORT:-"8332"}
 export BITCOIN_ZMQ_PORT=${BITCOIN_ZMQ_PORT:-"18543"}
 export BITCOIN_RPC_USER=${BITCOIN_RPC_USER:-"bitcoin"}
 export BITCOIN_RPC_PASSWD=${BITCOIN_RPC_PASSWD:-"abcd1234"}
+export BITCOIN_DB_CACHE=${BITCOIN_DB_CACHE:-"12000"}
 
 export BITCOIN_HOME="/usr/local/bitcoind"
 _main() {
@@ -16,7 +17,7 @@ _main() {
   # [core]
   blocksdir=$BITCOIN_HOME/blocksdir
   datadir=$BITCOIN_HOME/datadir
-  dbcache=14000
+  dbcache=$BITCOIN_DB_CACHE
   txindex=1
   pid=/bitcoind.pid
   daemon=0
@@ -26,12 +27,13 @@ _main() {
   rest=1
   rpcuser=$BITCOIN_RPC_USER
   rpcpassword=$BITCOIN_RPC_PASSWD
-
-  # [wallet]
-  disablewallet=1
   rpcport=$BITCOIN_RPC_PORT
   rpcallowip=0.0.0.0/0
   rpcallowip=::/0
+  rpcbind=0.0.0.0:$BITCOIN_RPC_PORT
+
+  # [wallet]
+  disablewallet=1
   listen=1
   discover=0
   dns=0
